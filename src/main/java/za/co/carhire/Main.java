57e9be7791ae371bf1c66f109e037f9e1b3546c8
lisazumana03 <zumanalisa@gmail.com>
2025-05-08 12:07:59 +0200

    file incoming

---
+++ src/main/java/za/co/carhire/Main.java
@@ -0,0 +1,7 @@
+package za.co.carhire;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("LG's car hire is open for business.");
+    }
+}
---
+++ src/main/java/za/co/carhire/domain/reservation/Booking.java
@@ -0,0 +1,11 @@
+package za.co.carhire.domain.reservation;
+
+/*
+Lisakhanya Zumana - 230864821
+Date: 08 May 2025
+ */
+
+public class Booking {
+    private int bookingID;
+    private int customerID;
+}
