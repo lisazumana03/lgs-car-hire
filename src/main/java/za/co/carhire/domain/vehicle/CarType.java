2d36221706a2dd786a0e32cf0c23fef234178c1a
ImtiyaazWaggie <waggieimtiyaaz@gmail.com>
2025-05-11 23:03:45 +0200

    create package and moved classes to it

--- src/main/java/za/co/carhire/domain/vehicle/CarType.java
+++ src/main/java/za/co/carhire/domain/vehicle/CarType.java
@@ -1,78 +1,92 @@
 package za.co.carhire.domain.vehicle;
 
-// Imtiyaaz Waggie 219374759//
-//date:10/05/2025 /
+/*
+Imtiyaaz Waggie - 219374759
+Date: 10/05/2025
+ */
 
 public class CarType {
+    // Primary attributes
     private int carTypeID;
-    private int carID;
     private String type;
     private String fuelType;
 
-    // Constructor
+    // Relationship attributes
+    private Car car;  // One-to-one relationship with Car
+
+    // Default constructor
     public CarType() {}
 
-    public CarType(int carTypeID, int carID, String type, String fuelType) {
-        this.carTypeID = carTypeID;
-        this.carID = carID;
-        this.type = type;
-        this.fuelType = fuelType;
+    // Private constructor for Builder pattern
+    private CarType(Builder builder) {
+        this.carTypeID = builder.carTypeID;
+        this.type = builder.type;
+        this.fuelType = builder.fuelType;
+        this.car = builder.car;
     }
 
-    // Getters and Setters
-
-    public int getCarTypeID() {
-        return carTypeID;
-    }
-
-    public void setCarTypeID(int carTypeID) {
-        this.carTypeID = carTypeID;
-    }
-
-    public int getCarID() {
-        return carID;
-    }
-
-    public void setCarID(int carID) {
-        this.carID = carID;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getFuelType() {
-        return fuelType;
-    }
-
-    public void setFuelType(String fuelType) {
-        this.fuelType = fuelType;
-    }
-
-    // Methods from the diagram
+    // Methods
     public void addCarDescription(String description) {
-        // Implementation for adding car description
-        // This might involve updating the type or additional properties
-        this.type = description;
+        // Implementation
     }
 
     public void updateCarDescription(boolean status) {
-        // Implementation for updating car description status
-        // This method might need additional parameters or logic
+        // Implementation
     }
+
+    // Getters
+    public int getCarTypeID() { return carTypeID; }
+    public String getType() { return type; }
+    public String getFuelType() { return fuelType; }
+    public Car getCar() { return car; }
 
     @Override
     public String toString() {
         return "CarType{" +
                 "carTypeID=" + carTypeID +
-                ", carID=" + carID +
                 ", type='" + type + '\'' +
                 ", fuelType='" + fuelType + '\'' +
+                ", car=" + car +
                 '}';
     }
-}
 
+    // Builder class
+    public static class Builder {
+        private int carTypeID;
+        private String type;
+        private String fuelType;
+        private Car car;
+
+        public Builder setCarTypeID(int carTypeID) {
+            this.carTypeID = carTypeID;
+            return this;
+        }
+
+        public Builder setType(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public Builder setFuelType(String fuelType) {
+            this.fuelType = fuelType;
+            return this;
+        }
+
+        public Builder setCar(Car car) {
+            this.car = car;
+            return this;
+        }
+
+        public Builder copy(CarType carType) {
+            this.carTypeID = carType.getCarTypeID();
+            this.type = carType.getType();
+            this.fuelType = carType.getFuelType();
+            this.car = carType.getCar();
+            return this;
+        }
+
+        public CarType build() {
+            return new CarType(this);
+        }
+    }
+}
