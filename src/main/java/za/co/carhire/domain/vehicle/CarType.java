84bbedbd7f4814b88d6662855db561f5a485e772
ImtiyaazWaggie <waggieimtiyaaz@gmail.com>
2025-05-11 23:20:43 +0200

    new cartype

--- src/main/java/za/co/carhire/domain/vehicle/CarType.java
+++ src/main/java/za/co/carhire/domain/vehicle/CarType.java
@@ -1,47 +1,95 @@
 package za.co.carhire.domain.vehicle;
 
-/*
-Imtiyaaz Waggie - 219374759
-Date: 10/05/2025
- */
-
 public class CarType {
-    // Primary attributes
     private int carTypeID;
+    private Car car;
     private String type;
     private String fuelType;
+    private int numberOfWheels;
+    private int numberOfSeats;
 
-    // Relationship attributes
-    private Car car;
+    // Empty constructor
+    public CarType() {}
 
+    // Constructor with all fields
+    public CarType(int carTypeID, Car car, String type, String fuelType, int numberOfWheels, int numberOfSeats) {
+        this.carTypeID = carTypeID;
+        this.car = car;
+        this.type = type;
+        this.fuelType = fuelType;
+        this.numberOfWheels = numberOfWheels;
+        this.numberOfSeats = numberOfSeats;
+    }
 
-
-    // Private constructor for Builder pattern
+    // Constructor using Builder
     private CarType(Builder builder) {
         this.carTypeID = builder.carTypeID;
+        this.car = builder.car;
         this.type = builder.type;
         this.fuelType = builder.fuelType;
-        this.car = builder.car;
+        this.numberOfWheels = builder.numberOfWheels;
+        this.numberOfSeats = builder.numberOfSeats;
     }
 
-    // Methods
-    public void addCarDescription(String description) {
-        // Implementation
+    // Builder class
+    public static class Builder {
+        private int carTypeID;
+        private Car car;
+        private String type;
+        private String fuelType;
+        private int numberOfWheels;
+        private int numberOfSeats;
+
+        public Builder setCarTypeID(int carTypeID) {
+            this.carTypeID = carTypeID;
+            return this;
+        }
+
+        public Builder setCar(Car car) {
+            this.car = car;
+            return this;
+        }
+
+        public Builder setType(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public Builder setFuelType(String fuelType) {
+            this.fuelType = fuelType;
+            return this;
+        }
+
+        public Builder setNumberOfWheels(int numberOfWheels) {
+            this.numberOfWheels = numberOfWheels;
+            return this;
+        }
+
+        public Builder setNumberOfSeats(int numberOfSeats) {
+            this.numberOfSeats = numberOfSeats;
+            return this;
+        }
+
+        public CarType build() {
+            return new CarType(this);
+        }
     }
 
-    public void updateCarDescription(boolean status) {
-        // Implementation
-    }
-
-    // Getters
-
-
+    // Getters and Setters
     public int getCarTypeID() {
         return carTypeID;
     }
 
     public void setCarTypeID(int carTypeID) {
         this.carTypeID = carTypeID;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
     }
 
     public String getType() {
@@ -60,61 +108,40 @@ public class CarType {
         this.fuelType = fuelType;
     }
 
-    public Car getCar() {
-        return car;
+    public int getNumberOfWheels() {
+        return numberOfWheels;
     }
 
-    public void setCar(Car car) {
-        this.car = car;
+    public void setNumberOfWheels(int numberOfWheels) {
+        this.numberOfWheels = numberOfWheels;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    // Methods from diagram
+    public void addCarDescription(String description) {
+        // Implementation for adding car description
+    }
+
+    public void updateCarDescription(String description, boolean update) {
+        // Implementation for updating car description
     }
 
     @Override
     public String toString() {
         return "CarType{" +
                 "carTypeID=" + carTypeID +
+                ", car=" + (car != null ? car.getCarID() : "null") +
                 ", type='" + type + '\'' +
                 ", fuelType='" + fuelType + '\'' +
-                ", car=" + car +
+                ", numberOfWheels=" + numberOfWheels +
+                ", numberOfSeats=" + numberOfSeats +
                 '}';
-    }
-
-    // Builder class
-    public static class Builder {
-        private int carTypeID;
-        private String type;
-        private String fuelType;
-        private Car car;
-
-        public Builder setCarTypeID(int carTypeID) {
-            this.carTypeID = carTypeID;
-            return this;
-        }
-
-        public Builder setType(String type) {
-            this.type = type;
-            return this;
-        }
-
-        public Builder setFuelType(String fuelType) {
-            this.fuelType = fuelType;
-            return this;
-        }
-
-        public Builder setCar(Car car) {
-            this.car = car;
-            return this;
-        }
-
-        public Builder copy(CarType carType) {
-            this.carTypeID = carType.getCarTypeID();
-            this.type = carType.getType();
-            this.fuelType = carType.getFuelType();
-            this.car = carType.getCar();
-            return this;
-        }
-
-        public CarType build() {
-            return new CarType(this);
-        }
     }
 }
