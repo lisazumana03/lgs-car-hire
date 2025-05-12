41a0eb4990d848a160360e14be2aa288122f9d6f
lisazumana03 <zumanalisa@gmail.com>
2025-05-12 21:08:43 +0200

    file incoming

--- src/main/java/za/co/carhire/util/Helper.java
+++ src/main/java/za/co/carhire/util/Helper.java
@@ -7,5 +7,11 @@ public class Helper {
         return s == null || s.isEmpty() || s.length() == 3;
     }
     //Email Validator
+    public static boolean isValidPostalCode(short postalCode) {
+        if (postalCode < -1 || postalCode > 9999) {
+            return false;
+        }
+        return true;
+    }
     //Date Validator
 }
