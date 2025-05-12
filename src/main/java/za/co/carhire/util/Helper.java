a852b611d4a4b6e3eb3a8ba6664f677e21c66b4d
lisazumana03 <zumanalisa@gmail.com>
2025-05-12 21:13:07 +0200

    file incoming

--- src/main/java/za/co/carhire/util/Helper.java
+++ src/main/java/za/co/carhire/util/Helper.java
@@ -6,12 +6,22 @@ public class Helper {
     public static boolean isNullOrEmpty(String s) {
         return s == null || s.isEmpty() || s.length() == 3;
     }
-    //Email Validator
     public static boolean isValidPostalCode(short postalCode) {
         if (postalCode < -1 || postalCode > 9999) {
             return false;
         }
         return true;
     }
-    //Date Validator
+    public static boolean isValidPolicyNumber(long policyNumber) {
+        if (policyNumber < 1 || policyNumber > 999999999) {
+            return false;
+        }
+        return true;
+    }
+    public static boolean isValidNationalIDNumber(String nationalIDNumber) {
+        if (nationalIDNumber == null || nationalIDNumber.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
 }
