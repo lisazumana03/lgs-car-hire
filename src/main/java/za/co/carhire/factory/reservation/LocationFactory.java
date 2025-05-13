27f472aa8689ea4ce9d9702f1f87729abf22da50
lisazumana03 <zumanalisa@gmail.com>
2025-05-13 22:22:13 +0200

    file incoming

--- src/main/java/za/co/carhire/factory/reservation/LocationFactory.java
+++ src/main/java/za/co/carhire/factory/reservation/LocationFactory.java
@@ -5,5 +5,22 @@ Lisakhanya Zumana (230864821)
 Date: 12 May 2025
  */
 
+import za.co.carhire.domain.reservation.Location;
+import za.co.carhire.util.Helper;
+
 public class LocationFactory {
+    public static Location createLocation(int locationID, String locationName, String streetName, String city, String provinceOrState, String country, Short postalCode) {
+        if(Helper.isNullOrEmpty(locationName)){
+            return null;
+        }
+        return new Location.Builder()
+                .setLocationID(locationID)
+                .setLocationName(locationName)
+                .setStreetName(streetName)
+                .setCity(city)
+                .setProvinceOrState(provinceOrState)
+                .setCountry(country)
+                .setPostalCode(postalCode)
+                .build();
+    }
 }
