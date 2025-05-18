30078e0407b945cfbee095bf38fe0b8d41a3985c
NohambaS <164313427+NohambaS@users.noreply.github.com>
2025-05-18 20:25:31 +0200

    Add files via upload

---
+++ src/test/java/za/co/carhire/factory/reservation/MaintenanceFactoryTest.java
@@ -0,0 +1,44 @@
+package za.ac.cput.factory;
+
+/*
+ * Sibulele Gift Nohamba (220374686)
+ * Date: 25/03/2025
+ * */
+
+import org.junit.jupiter.api.Test;
+import za.ac.cput.domain.Maintenance;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaintenanceFactoryTest {
+
+    private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+    @Test
+    void createMaintenance() throws ParseException {
+
+        Date serviceDate = dateFormat.parse("2025-3-12");
+        Maintenance maintenance = new Maintenance.Builder()
+                .setMaintenanceID(24)
+                .setCarID(54)
+                .setServiceDate(serviceDate)
+                .setDescription("Minor service")
+                .setCost(350.00)
+                .setStatus("In the workshop")
+                .setMechanic("Alvin Lewis")
+                .build();
+
+        assertEquals(24, maintenance.getMaintenanceId());
+        assertEquals(54, maintenance.getCarID());
+        assertEquals(serviceDate, maintenance.getServiceDate());
+        assertEquals("Minor service", maintenance.getDescription());
+        assertEquals(350.00, maintenance.getCost());
+        assertEquals("In the workshop", maintenance.getStatus());
+        assertEquals("Alvin Lewis", maintenance.getMechanic());
+        assertTrue(true);
+        assertNotNull(maintenance);
+
+    }
+}
