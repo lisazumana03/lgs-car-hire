8062553a43297f276bef670218363f35ec4d07f8
lisazumana03 <zumanalisa@gmail.com>
2025-05-19 09:10:57 +0200

    file incoming

---
+++ src/main/java/za/co/carhire/LGsCarHireSpringMain.java
@@ -0,0 +1,11 @@
+package za.co.carhire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LGsCarHireSpringMain {
+    public static void main(String[] args) {
+        SpringApplication.run(LGsCarHireSpringMain.class, args);
+    }
+}
---
+++ src/main/java/za/co/carhire/repository/reservation/IBookingRepository.java
@@ -0,0 +1,19 @@
+package za.co.carhire.repository.reservation;
+
+/*
+Lisakhanya Zumana (230864821)
+Date: 19/05/2025
+ */
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.co.carhire.domain.reservation.Booking;
+import za.co.carhire.domain.reservation.Location;
+
+import java.util.List;
+
+@Repository
+public interface IBookingRepository extends JpaRepository<Booking, Long> {
+    List<Booking> findByPickupLocation(Location pickupLocation);
+    List<Booking> findByDropOffLocation(Location dropOffLocation);
+}
