9afedec58be263777b167707d89566508ad4c470
lisazumana03 <zumanalisa@gmail.com>
2025-05-25 00:00:44 +0200

    file incoming

--- src/main/java/za/co/carhire/repository/reservation/IMaintenanceRepository.java
+++ src/main/java/za/co/carhire/repository/reservation/IMaintenanceRepository.java
@@ -6,7 +6,7 @@ Date: 24/05/2025
  */
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import za.co.carhire.domain.reservation.Maintence;
+import za.co.carhire.domain.reservation.Maintenance;
 
 public interface IMaintenanceRepository extends JpaRepository<Maintenance, Integer> {
 }
