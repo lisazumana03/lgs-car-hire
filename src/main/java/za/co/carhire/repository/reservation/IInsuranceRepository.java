23368b4bfe48f19350d5b8ec08d3ca3c433a0df7
NohambaS <164313427+NohambaS@users.noreply.github.com>
2025-05-24 23:26:26 +0200

    Update IInsuranceRepository.java

--- src/main/java/za/co/carhire/repository/reservation/IInsuranceRepository.java
+++ src/main/java/za/co/carhire/repository/reservation/IInsuranceRepository.java
@@ -1,4 +1,8 @@
 package za.co.carhire.repository.reservation;
+/*
+Sibulele Gift Nohamba (220374686)
+Date: 24/05/2025
+ */
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import za.co.carhire.domain.reservation.Insurance;
