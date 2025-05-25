561bc51be6bf9aa77b577a30313d2f854238da5e
SaneleZondi <221602011@mycput.ac.za>
2025-05-25 22:30:10 +0200

    Added Payment and Invoice Service Classes

--- src/main/java/za/co/carhire/service/impl/reservation/InvoiceService.java
+++ src/main/java/za/co/carhire/service/impl/reservation/InvoiceService.java
@@ -1,4 +1,46 @@
 package za.co.carhire.service.impl.reservation;
 
-public class InvoiceService {
+/* InvoiceService.java
+ * Sanele Zondi (221602011)
+ * Due Date: 25/05/2025
+ * */
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.co.carhire.domain.reservation.*;
+import za.co.carhire.repository.reservation.IInvoiceRepository;
+import za.co.carhire.service.reservation.*;
+
+import java.util.Set;
+
+@Service
+public abstract class InvoiceService implements IInvoiceService {
+
+    @Autowired
+    private IInvoiceRepository invoiceRepository;
+
+    @Override
+    public Set<Invoice> getInvoices() {
+        return Set.of();
+    }
+
+    @Override
+    public Invoice create(Invoice invoice) {
+        return invoiceRepository.save(invoice);
+    }
+    @Override
+    public Invoice read(int invoiceID) {
+        return (Invoice) invoiceRepository.findById(invoiceID);
+    }
+    @Override
+    public Invoice update(Invoice invoice) {
+        if (invoiceRepository.existsById(invoice.getInvoiceID())) {
+            return invoiceRepository.save(invoice);
+        }
+        return null;
+    }
+    @Override
+    public void delete(int invoiceId) {
+        invoiceRepository.deleteById(invoiceId);
+    }
 }
