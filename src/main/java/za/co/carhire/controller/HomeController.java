eb934ee3ef252f8d492a67912b8a72cb4c04672e
lisazumana03 <zumanalisa@gmail.com>
2025-06-03 09:46:29 +0200

    file incoming

--- src/main/java/za/co/carhire/controller/HomeController.java
+++ src/main/java/za/co/carhire/controller/HomeController.java
@@ -1,4 +1,13 @@
 package za.co.carhire.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/lgs-car-hire")
 public class HomeController {
+    @RequestMapping("/main")
+    public String home() {
+        return "Welcome to the Car Hire System";
+    }
 }
