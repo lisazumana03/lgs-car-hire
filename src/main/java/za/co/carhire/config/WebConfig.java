0cc4720697e17c92937912e273d196e802dbe363
lisazumana03 <zumanalisa@gmail.com>
2025-07-28 10:18:22 +0200

    file incoming

--- src/main/java/za/co/carhire/config/WebConfig.java
+++ src/main/java/za/co/carhire/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/***")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3046")//React.js development server
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
