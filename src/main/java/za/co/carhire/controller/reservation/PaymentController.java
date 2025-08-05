1ee0ea53698d49c77cf78b36679051cd8faa477e
SaneleZondi <221602011@mycput.ac.za>
2025-08-05 07:18:21 +0200

    Updated PaymentController

--- src/main/java/za/co/carhire/controller/reservation/PaymentController.java
+++ src/main/java/za/co/carhire/controller/reservation/PaymentController.java
@@ -1,4 +1,42 @@
 package za.co.carhire.controller.reservation;
 
+/*PaymentController.java
+ * Payment Controller class
+ * Sanele Zondi (221602011)
+ * */
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import za.co.carhire.domain.reservation.Payment;
+import za.co.carhire.service.reservation.impl.PaymentService;
+
+@RestController
+@RequestMapping("/payment")
+@CrossOrigin(origins = "*")
 public class PaymentController {
+    @Autowired
+    private PaymentService paymentService;
+
+    @PostMapping("/create")
+    public ResponseEntity<Payment> create(@RequestBody Payment payment) {
+        return new ResponseEntity<>(paymentService.create(payment), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/read/{id}")
+    public Payment read(@PathVariable int id) {
+        return paymentService.read(id);
+    }
+
+    @PutMapping("/update")
+    public Payment update(@RequestBody Payment payment) {
+        return paymentService.update(payment);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Payment> delete(@PathVariable int id) {
+        paymentService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
