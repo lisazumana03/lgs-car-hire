96281ad26c8897810e2d11e074594898a28d6c7d
Olox04 <tshingoo@gmail.com>
2025-08-07 02:14:48 +0200

    added Service & Controller

--- src/main/java/za/co/carhire/factory/reservation/SupportTicketFactory.java
+++ src/main/java/za/co/carhire/factory/reservation/SupportTicketFactory.java
@@ -1,4 +1,5 @@
 package za.co.carhire.factory.reservation;
+import za.co.carhire.domain.authentication.User;
 import za.co.carhire.domain.reservation.SupportTicket;
 import za.co.carhire.util.Helper;
 
@@ -10,20 +11,18 @@ Date: 14 May 2025
 public class SupportTicketFactory {
 
     //Support Submission
-    public static SupportTicket submitTicket(int ticketID, int userID, String message){
+    public static SupportTicket submitTicket(int ticketID, User user, String message){
         if(Helper.isWithinBoundary(ticketID)||
-           Helper.isWithinBoundary(userID)||
-           Helper.isNullOrEmpty(message)){
+           Helper.isEmptyOrNull(message)){
             return null;
         }
         else{
             return new SupportTicket.Builder()
                     .setTicketID(ticketID)
-                    .setUserID(userID)
+                    .setUser(user)
                     .setMessage(message)
                     .setStatus("Open")
                     .setResponse(0)
-//                    .setCreatedAt(new Date())
                     .build();
         }
     }
--- src/main/java/za/co/carhire/repository/feedback/IReviewRepository.java
+++ src/main/java/za/co/carhire/repository/feedback/IReviewRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import za.co.carhire.domain.feedback.Review;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface IReviewRepository extends JpaRepository<Review, Integer> {
+    Optional<Review> findById(int reviewID);
+    List<Review> findByRating(int rating);
+    List<Review> findAll();
 }
