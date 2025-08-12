3a31a79e613c42164fa87cafcaa86b62a7576b82
lisazumana03 <zumanalisa@gmail.com>
2025-08-12 15:03:06 +0200

    file incoming

--- src/main/java/za/co/carhire/controller/reservation/MaintenanceController.java
+++ src/main/java/za/co/carhire/controller/reservation/MaintenanceController.java
@@ -1,13 +1,28 @@
 package za.co.carhire.controller.reservation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import za.co.carhire.domain.reservation.Maintenance;
 import za.co.carhire.service.reservation.impl.MaintenanceService;
+
+/*
+Sibulele Gift Nohamba
+220374686
+Date: 12/08/2025
+ */
 
 @RestController
 @RequestMapping("/maintenance")
 public class MaintenanceController {
     @Autowired
     private MaintenanceService maintenanceService;
+
+    @PostMapping("/create")
+    public ResponseEntity<Maintenance> create(@RequestBody Maintenance maintenance) {
+        return ResponseEntity.ok(maintenanceService.create(maintenance));
+    }
 }
