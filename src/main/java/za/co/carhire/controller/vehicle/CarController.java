38328064e4fac59b166630dd3c3807b6af92c6a8
Imtiyaaz Waggie <WaggieImtiyaaz@gmail.com>
2025-08-29 18:38:39 +0200

    Add /api prefix to Car and CarType controller endpoints

--- src/main/java/za/co/carhire/controller/vehicle/CarController.java
+++ src/main/java/za/co/carhire/controller/vehicle/CarController.java
@@ -15,11 +15,12 @@ import java.util.stream.Collectors;
 /*
 Imtiyaaz Waggie 219374759
 Date: 25/05/2025
+Updated: 28/08/2025 - Added /api prefix to endpoints
  */
 
 @RestController
-@RequestMapping("/car")
-@CrossOrigin(origins = "http://localhost:3046")
+@RequestMapping("/api/car") 
+// @CrossOrigin(origins = "http://localhost:3046")
 public class CarController {
     @Autowired
     private ICarService carService;
