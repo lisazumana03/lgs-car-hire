c88f82f4909b3294561037bb97c61183d0dfd1fa
Imtiyaaz Waggie <WaggieImtiyaaz@gmail.com>
2025-08-31 12:43:32 +0200

    Add image URL support to Car, CarDTO, CarFactory, and CarMapper classes

--- src/main/java/za/co/carhire/dto/vehicle/CarDTO.java
+++ src/main/java/za/co/carhire/dto/vehicle/CarDTO.java
@@ -6,30 +6,31 @@ import java.io.Serializable;
  * Data Transfer Object for Car entity with full CarType details
  * Author: Imtiyaaz Waggie 219374759
  * Date: 28/08/2025
- * Updated: Added full CarType details
+ * Updated: 31/08/2025 - Added image URL support
  */
 public class CarDTO implements Serializable {
-    
+
     private int carID;
     private String model;
     private String brand;
     private int year;
     private boolean availability;
     private double rentalPrice;
-    
+    private String imageUrl;
+
     private Integer carTypeID;
     private String carTypeName;
     private String carTypeFuelType;
     private Integer carTypeNumberOfWheels;
     private Integer carTypeNumberOfSeats;
-    
+
     private Integer insuranceID;
     private Integer bookingID;
-    
+
     public CarDTO() {
     }
-    
-    public CarDTO(int carID, String model, String brand, int year, 
+
+    public CarDTO(int carID, String model, String brand, int year,
                   boolean availability, double rentalPrice) {
         this.carID = carID;
         this.model = model;
@@ -38,10 +39,10 @@ public class CarDTO implements Serializable {
         this.availability = availability;
         this.rentalPrice = rentalPrice;
     }
-    
-    public CarDTO(int carID, String model, String brand, int year, 
-                  boolean availability, double rentalPrice,
-                  Integer carTypeID, String carTypeName, 
+
+    public CarDTO(int carID, String model, String brand, int year,
+                  boolean availability, double rentalPrice, String imageUrl,
+                  Integer carTypeID, String carTypeName,
                   Integer insuranceID, Integer bookingID) {
         this.carID = carID;
         this.model = model;
@@ -49,12 +50,13 @@ public class CarDTO implements Serializable {
         this.year = year;
         this.availability = availability;
         this.rentalPrice = rentalPrice;
+        this.imageUrl = imageUrl;
         this.carTypeID = carTypeID;
         this.carTypeName = carTypeName;
         this.insuranceID = insuranceID;
         this.bookingID = bookingID;
     }
-    
+
     public static class Builder {
         private int carID;
         private String model;
@@ -62,6 +64,7 @@ public class CarDTO implements Serializable {
         private int year;
         private boolean availability;
         private double rentalPrice;
+        private String imageUrl;
         private Integer carTypeID;
         private String carTypeName;
         private String carTypeFuelType;
@@ -69,72 +72,77 @@ public class CarDTO implements Serializable {
         private Integer carTypeNumberOfSeats;
         private Integer insuranceID;
         private Integer bookingID;
-        
+
         public Builder setCarID(int carID) {
             this.carID = carID;
             return this;
         }
-        
+
         public Builder setModel(String model) {
             this.model = model;
             return this;
         }
-        
+
         public Builder setBrand(String brand) {
             this.brand = brand;
             return this;
         }
-        
+
         public Builder setYear(int year) {
             this.year = year;
             return this;
         }
-        
+
         public Builder setAvailability(boolean availability) {
             this.availability = availability;
             return this;
         }
-        
+
         public Builder setRentalPrice(double rentalPrice) {
             this.rentalPrice = rentalPrice;
             return this;
         }
-        
+
+        public Builder setImageUrl(String imageUrl) {
+            this.imageUrl = imageUrl;
+            return this;
+        }
+
         public Builder setCarTypeID(Integer carTypeID) {
             this.carTypeID = carTypeID;
             return this;
         }
-        
+
         public Builder setCarTypeName(String carTypeName) {
             this.carTypeName = carTypeName;
             return this;
         }
-        
+
         public Builder setCarTypeFuelType(String carTypeFuelType) {
             this.carTypeFuelType = carTypeFuelType;
             return this;
         }
-        
+
         public Builder setCarTypeNumberOfWheels(Integer carTypeNumberOfWheels) {
             this.carTypeNumberOfWheels = carTypeNumberOfWheels;
             return this;
         }
-        
+
         public Builder setCarTypeNumberOfSeats(Integer carTypeNumberOfSeats) {
             this.carTypeNumberOfSeats = carTypeNumberOfSeats;
             return this;
         }
-        
+
         public Builder setInsuranceID(Integer insuranceID) {
             this.insuranceID = insuranceID;
             return this;
         }
-        
+
         public Builder setBookingID(Integer bookingID) {
             this.bookingID = bookingID;
             return this;
         }
-        
+
         public CarDTO build() {
             CarDTO dto = new CarDTO();
             dto.carID = this.carID;
@@ -143,6 +151,7 @@ public class CarDTO implements Serializable {
             dto.year = this.year;
             dto.availability = this.availability;
             dto.rentalPrice = this.rentalPrice;
+            dto.imageUrl = this.imageUrl;
             dto.carTypeID = this.carTypeID;
             dto.carTypeName = this.carTypeName;
             dto.carTypeFuelType = this.carTypeFuelType;
@@ -153,111 +162,119 @@ public class CarDTO implements Serializable {
             return dto;
         }
     }
-    
+
     public int getCarID() {
         return carID;
     }
-    
+
     public void setCarID(int carID) {
         this.carID = carID;
     }
-    
+
     public String getModel() {
         return model;
     }
-    
+
     public void setModel(String model) {
         this.model = model;
     }
-    
+
     public String getBrand() {
         return brand;
     }
-    
+
     public void setBrand(String brand) {
         this.brand = brand;
     }
-    
+
     public int getYear() {
         return year;
     }
-    
+
     public void setYear(int year) {
         this.year = year;
     }
-    
+
     public boolean isAvailability() {
         return availability;
     }
-    
+
     public void setAvailability(boolean availability) {
         this.availability = availability;
     }
-    
+
     public double getRentalPrice() {
         return rentalPrice;
     }
-    
+
     public void setRentalPrice(double rentalPrice) {
         this.rentalPrice = rentalPrice;
     }
-    
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
     public Integer getCarTypeID() {
         return carTypeID;
     }
-    
+
     public void setCarTypeID(Integer carTypeID) {
         this.carTypeID = carTypeID;
     }
-    
+
     public String getCarTypeName() {
         return carTypeName;
     }
-    
+
     public void setCarTypeName(String carTypeName) {
         this.carTypeName = carTypeName;
     }
-    
+
     public String getCarTypeFuelType() {
         return carTypeFuelType;
     }
-    
+
     public void setCarTypeFuelType(String carTypeFuelType) {
         this.carTypeFuelType = carTypeFuelType;
     }
-    
+
     public Integer getCarTypeNumberOfWheels() {
         return carTypeNumberOfWheels;
     }
-    
+
     public void setCarTypeNumberOfWheels(Integer carTypeNumberOfWheels) {
         this.carTypeNumberOfWheels = carTypeNumberOfWheels;
     }
-    
+
     public Integer getCarTypeNumberOfSeats() {
         return carTypeNumberOfSeats;
     }
-    
+
     public void setCarTypeNumberOfSeats(Integer carTypeNumberOfSeats) {
         this.carTypeNumberOfSeats = carTypeNumberOfSeats;
     }
-    
+
     public Integer getInsuranceID() {
         return insuranceID;
     }
-    
+
     public void setInsuranceID(Integer insuranceID) {
         this.insuranceID = insuranceID;
     }
-    
+
     public Integer getBookingID() {
         return bookingID;
     }
-    
+
     public void setBookingID(Integer bookingID) {
         this.bookingID = bookingID;
     }
-    
+
     @Override
     public String toString() {
         return "CarDTO{" +
@@ -267,6 +284,7 @@ public class CarDTO implements Serializable {
                 ", year=" + year +
                 ", availability=" + availability +
                 ", rentalPrice=" + rentalPrice +
+                ", imageUrl='" + imageUrl + '\'' +
                 ", carTypeID=" + carTypeID +
                 ", carTypeName='" + carTypeName + '\'' +
                 ", carTypeFuelType='" + carTypeFuelType + '\'' +
--- src/main/java/za/co/carhire/factory/vehicle/CarFactory.java
+++ src/main/java/za/co/carhire/factory/vehicle/CarFactory.java
@@ -2,6 +2,7 @@ package za.co.carhire.factory.vehicle;
 
 // Imtiyaaz Waggie 219374759//
 //date:10/05/2025 //
+// Updated: 31/08/2025 - Added image URL support
 
 import za.co.carhire.domain.reservation.Insurance;
 import za.co.carhire.domain.reservation.Booking;
@@ -9,7 +10,6 @@ import za.co.carhire.domain.vehicle.Car;
 import za.co.carhire.domain.vehicle.CarType;
 
 public class CarFactory {
-
 
     public static Car createBasicCar(int carID, String model, String brand, int year, double rentalPrice) {
         return new Car.Builder()
@@ -22,6 +22,18 @@ public class CarFactory {
                 .build();
     }
 
+    public static Car createBasicCarWithImage(int carID, String model, String brand, int year,
+                                              double rentalPrice, String imageUrl) {
+        return new Car.Builder()
+                .setCarID(carID)
+                .setModel(model)
+                .setBrand(brand)
+                .setYear(year)
+                .setRentalPrice(rentalPrice)
+                .setImageUrl(imageUrl)
+                .setAvailability(true)
+                .build();
+    }
 
     public static Car createCompleteCar(int carID, String model, String brand, int year,
                                         boolean availability, double rentalPrice,
@@ -33,6 +45,23 @@ public class CarFactory {
                 .setYear(year)
                 .setAvailability(availability)
                 .setRentalPrice(rentalPrice)
+                .setCarType(carType)
+                .setInsurance(insurance)
+                .setBooking(booking)
+                .build();
+    }
+
+    public static Car createCompleteCarWithImage(int carID, String model, String brand, int year,
+                                                 boolean availability, double rentalPrice, String imageUrl,
+                                                 CarType carType, Insurance insurance, Booking booking) {
+        return new Car.Builder()
+                .setCarID(carID)
+                .setModel(model)
+                .setBrand(brand)
+                .setYear(year)
+                .setAvailability(availability)
+                .setRentalPrice(rentalPrice)
+                .setImageUrl(imageUrl)
                 .setCarType(carType)
                 .setInsurance(insurance)
                 .setBooking(booking)
@@ -52,6 +81,19 @@ public class CarFactory {
                 .build();
     }
 
+    public static Car createCarWithTypeAndImage(int carID, String model, String brand, int year,
+                                                double rentalPrice, String imageUrl, CarType carType) {
+        return new Car.Builder()
+                .setCarID(carID)
+                .setModel(model)
+                .setBrand(brand)
+                .setYear(year)
+                .setRentalPrice(rentalPrice)
+                .setImageUrl(imageUrl)
+                .setCarType(carType)
+                .setAvailability(true)
+                .build();
+    }
 
     public static Car createCarWithInsurance(int carID, String model, String brand, int year,
                                              double rentalPrice, CarType carType, Insurance insurance) {
@@ -67,6 +109,21 @@ public class CarFactory {
                 .build();
     }
 
+    public static Car createCarWithInsuranceAndImage(int carID, String model, String brand, int year,
+                                                     double rentalPrice, String imageUrl,
+                                                     CarType carType, Insurance insurance) {
+        return new Car.Builder()
+                .setCarID(carID)
+                .setModel(model)
+                .setBrand(brand)
+                .setYear(year)
+                .setRentalPrice(rentalPrice)
+                .setImageUrl(imageUrl)
+                .setCarType(carType)
+                .setInsurance(insurance)
+                .setAvailability(true)
+                .build();
+    }
 
     public static Car createCarCopy(Car originalCar, int newCarID) {
         return new Car.Builder()
@@ -74,7 +131,6 @@ public class CarFactory {
                 .setCarID(newCarID)
                 .build();
     }
-
 
     public static Car createUnavailableCar(int carID, String model, String brand, int year,
                                            double rentalPrice, CarType carType) {
@@ -89,4 +145,17 @@ public class CarFactory {
                 .build();
     }
 
+    public static Car createUnavailableCarWithImage(int carID, String model, String brand, int year,
+                                                    double rentalPrice, String imageUrl, CarType carType) {
+        return new Car.Builder()
+                .setCarID(carID)
+                .setModel(model)
+                .setBrand(brand)
+                .setYear(year)
+                .setRentalPrice(rentalPrice)
+                .setImageUrl(imageUrl)
+                .setCarType(carType)
+                .setAvailability(false)
+                .build();
+    }
 }
